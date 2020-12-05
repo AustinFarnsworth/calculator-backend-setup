abeba0b8426da5f7a1e41a923f24c5ac35971cfc
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2020-12-05 11:45:47 -0700

    first-commit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res) {
+    res.send("Thanks for posting that!");
+})
+
+app.listen(3000, function(){
+    console.log("Server Started!");
+})
